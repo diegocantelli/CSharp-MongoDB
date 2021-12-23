821be069e0668acefecb98e890aa999820c33498
Diego Cantelli <diegocantelli19@gmail.com>
2021-12-23 19:59:29 -0300

    Criando um array BsonArray

--- mongodb/mongodb/ManipulandoDocumentos.cs
+++ mongodb/mongodb/ManipulandoDocumentos.cs
@@ -23,6 +23,14 @@ namespace mongodb
             doc.Add("Ano", 1999);
             doc.Add("Páginas", 899);
 
+            //Criando um array no json
+            var assuntoArray = new BsonArray();
+            assuntoArray.Add("Fantasia");
+            assuntoArray.Add("Ação");
+
+            //add o array ao documento principal
+            doc.Add("Assunto", assuntoArray);
+
             Console.WriteLine(doc);
         }
     }
