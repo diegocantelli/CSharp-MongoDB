494753ed6c08e66f9d05460b58941916d96e47ad
Diego Cantelli <diegocantelli19@gmail.com>
2021-12-23 19:56:26 -0300

    Criando um documento json e adicionando novas propriedades

---
+++ mongodb/mongodb/ManipulandoDocumentos.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mongodb
+{
+    public class ManipulandoDocumentos
+    {
+
+        public static async Task AsyncMain() 
+        {
+            //Criando um documento
+            var doc = new BsonDocument
+            {
+                {"Título", "Guerra dos Tronos" }
+            };
+
+            //Adicionand uma propriedade ao documento
+            doc.Add("Autor", "George R R Martin");
+            doc.Add("Ano", 1999);
+            doc.Add("Páginas", 899);
+
+            Console.WriteLine(doc);
+        }
+    }
+}
--- mongodb/mongodb/Program.cs
+++ mongodb/mongodb/Program.cs
@@ -6,7 +6,8 @@ namespace mongodb
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t = ManipulandoDocumentos.AsyncMain();
+            Console.ReadLine();
         }
     }
 }
